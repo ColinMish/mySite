94009e0fe992677dc5e6beb5323d5ce4cae7c7cc
ColinMish <colin.mackey92@gmail.com>
2015-09-04 12:52:45 +0100

    Added smooth link scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,17 @@ $(document).ready(function(){
 	$('#myPic').load(function(){
 		$('.jumbotron').fadeIn(1400);
 	});
+
+	$("#projectsButtonProjects, #projectsButtonGlyph").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#projects").offset().top
+	    }, 700);
+	});
+
+	$("#helloButtonColin, #helloButtonAbout").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#hello").offset().top
+	    }, 700);
+	});
 	
 });
